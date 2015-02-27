cd3d6ff4b49ac5c5c13038cf9412ca6d2bbae448
John Pinkerton <jpinkerton88@gmail.com>
2015-02-27 16:34:50 -0500

    add store and find functions

--- lib/barrel.rb
+++ lib/barrel.rb
@@ -4,4 +4,16 @@ require "barrel/store"
 
 module Barrel
 
+  def self.store(title, value)
+    store = Barrel::Store.find_or_initialize_by(title: title)
+    store.value = value
+
+    store.save
+  end
+
+  def self.find(title)
+    store = Barrel::Store.find_by(title: title)
+    store.nil? ? nil : store.value
+  end
+
 end
